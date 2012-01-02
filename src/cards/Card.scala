e71905d345b312735ee03bd42bd66c186c6095fa
User1 <User1@Notebook-Win7>
2012-01-02 23:21:20 +0100

    I didnt find a way to implement an unsigned int, so all values in class card have changed to long. two new methodes were created. first gives you the suit of a card. second gives you the rank of the card.

--- src/cards/Card.scala
+++ src/cards/Card.scala
@@ -1,5 +1,5 @@
 /**
- * 
+ *
  */
 package cards
 
@@ -12,6 +12,8 @@ package cards
  * @author Oliver Friedrich
  */
 object Card {
+
+  /*
 	val DiamondsSeven=0
 	val DiamondsEight=1
 	val DiamondsNine=2
@@ -48,17 +50,86 @@ object Card {
 	val ClubsKing=536870912
 	val ClubsAce=1073741824
 	
-	val DiamondsQuotient=0
-	val HeartsQuotient=1
-	val SpadesQuotient=2
-	val ClubsQuotient=3
-	
-	val SevenModulo=0
-	val EightModulo=1
-	val NineModulo=2
-	val TenModulo=3
-	val JackModulo=4
-	val QueenModulo=5
-	val KingModulo=6
-	val AceModulo=7
+	*/
+
+  val DiamondsSeven = 1L // 0
+  val DiamondsEight = 2L
+  val DiamondsNine = 4L
+  val DiamondsTen = 8L
+  val DiamondsJack = 16L
+  val DiamondsQueen = 32L
+  val DiamondsKing = 64L
+  val DiamondsAce = 128L // 7
+
+  val HeartsSeven = 256L // 8
+  val HeartsEight = 512L
+  val HeartsNine = 1024L
+  val HeartsTen = 2048L
+  val HeartsJack = 4096L
+  val HeartsQueen = 8192L
+  val HeartsKing = 16384L
+  val HeartsAce = 32768L // 15
+
+  val SpadesSeven = 65536L // 16
+  val SpadesEight = 131072L
+  val SpadesNine = 262144L
+  val SpadesTen = 524288L
+  val SpadesJack = 1048576L
+  val SpadesQueen = 2097152L
+  val SpadesKing = 4194304L
+  val SpadesAce = 8388608L // 23
+
+  val ClubsSeven = 16777216L // 24
+  val ClubsEight = 33554432L
+  val ClubsNine = 67108864L
+  val ClubsTen = 134217728L
+  val ClubsJack = 268435456L
+  val ClubsQueen = 536870912L
+  val ClubsKing = 1073741824L
+  val ClubsAce = 2147483648L // 31
+
+  val DiamondsQuotient = 0
+  val HeartsQuotient = 1
+  val SpadesQuotient = 2
+  val ClubsQuotient = 3
+
+  val SevenModulo = 0
+  val EightModulo = 1
+  val NineModulo = 2
+  val TenModulo = 3
+  val JackModulo = 4
+  val QueenModulo = 5
+  val KingModulo = 6
+  val AceModulo = 7
+  
+  /**
+   * returns the suit of a card.
+   * Thats a value between 0 and 3.
+   */
+  def getSuit(pCard:Long):Integer= {
+    var suit = 0
+    var card = pCard
+    while(card != 1) {
+      card = card >> 1
+      suit+=1
+    }
+    return (suit / 8);
+  }
+  /**
+   * returns the rank of a card.
+   * Thats a value between 0 and 7.
+   */
+  def getRank(pCard:Long):Integer= {
+    var rank = 0
+    var card = pCard
+    while(card != 1) {
+      card = card >> 1
+      rank+=1
+    }
+    return (rank % 8);
+  }
+  
+  
+  
+  
 }
