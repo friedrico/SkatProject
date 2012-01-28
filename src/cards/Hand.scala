757e283cb166d50ecd792e52b2a94980370ac2b3
Lars <Lars@Lars-Win7-PC>
2012-01-28 14:11:30 +0100

    changes in class hand comment for function "filter" changes in function "filter"

--- src/cards/Hand.scala
+++ src/cards/Hand.scala
@@ -21,76 +21,43 @@ class Hand extends CardCollection {
     trumpSuit = pTrump.getSuit
   }
   /**
-   * @return : only the diamond cards without the jacks.
+   * @param : the suit of a card.
+   * @return : the cards, which would match with this card.
    */
-  def getDiamonds(): Long = {
-    if ((vector & 0xEFL) == 0L) {
-      return vector
-    } else {
-      return (vector & 0xEFL)
-    }
-  }
-  /**
-   * @return : only the hearts cards without the jacks.
-   */
-  def getHearts(): Long = {
-    if ((vector & 0xEF00L) == 0L) {
-      return vector
-    } else {
-      return (vector & 0xEF00L)
-    }
-  }
-
-  def getSpades(): Long = {
-    if ((vector & 0xEF0000L) == 0L) {
-      return vector
-    } else {
-      return (vector & 0xEF0000L)
-    }
-  }
-
-  def getClubs(): Long = {
-    if ((vector & 0xEF000000L) == 0L) {
-      return vector
-    } else {
-      return (vector & 0xEF000000L)
-    }
-  }
-
   def filter(suit: Int): Long = {
-    // pruefen, ob suit = trumpf
-
+    // the card is a trump card.
     if (suit == trumpSuit) {
-      // return irgendwas
+      if ((vector & trumpMask) == 0L) {
+        return vector
+      }
+      return vector & trumpMask
     }
-    // kein trump
+
+    // the card is not a trump card.
     suit match {
       // TODO : constants for cases
       case 0 => // diamond
         if ((vector & 0xEFL) == 0L) {
           return vector
-        } else {
-          return (vector & 0xEFL)
         }
+        return (vector & 0xEFL)
       case 1 => // heart
         if ((vector & 0xEF00L) == 0L) {
           return vector
-        } else {
-          return (vector & 0xEF00L)
         }
+        return (vector & 0xEF00L)
       case 2 => // spades
         if ((vector & 0xEF0000L) == 0L) {
           return vector
-        } else {
-          return (vector & 0xEF0000L)
         }
+        return (vector & 0xEF0000L)
       case 3 => // clubs
         if ((vector & 0xEF000000L) == 0L) {
           return vector
-        } else {
-          return (vector & 0xEF000000L)
         }
+        return (vector & 0xEF000000L)
     }
+    // not implemented, so we return exit_failure
     return -1
   }
 
