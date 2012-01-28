f2b04a6e3f778e494b1a3e9bf76282396e42bd59
Lars <Lars@Lars-Win7-PC>
2012-01-28 14:35:04 +0100

    decimals to hex in class "Trump"

--- src/cards/Trump.scala
+++ src/cards/Trump.scala
@@ -8,8 +8,8 @@ package cards
  *
  */
 abstract case class Trump {
-  def getMask:Long
-  def getSuit:Int
+  def getMask: Long
+  def getSuit: Int
 }
 
 /**
@@ -17,123 +17,132 @@ abstract case class Trump {
  *
  */
 case class Diamonds extends Trump {
-  def getMask():Long= {
-    
-    //     00FF
-    // 10101010
-    // 101010FF
-    
+  def getMask(): Long = {
+
+    // 000000FF = Diamonds
+    // 10101010 = Jacks
+    // 101010FF = Diamonds | Jacks
+
     // 0000 0000 0000 0000 0000 0000 1111 1111 = 255L = Diamonds
     // 0001 0000 0001 0000 0001 0000 0001 0000 = 269488144L = Jacks
     // 0001 0000 0001 0000 0001 0000 1111 1111 = 269488383L = Diamonds | Jacks
-    return 269488383L
+    return 0x101010FFL
   }
-  
-  def getSuit():Int= {
+
+  def getSuit(): Int = {
     return 0
   }
-  override def toString="Diamonds"
+  override def toString = "Diamonds"
 }
 /**
  * @author Oliver Friedrich
  *
  */
 case class Hearts extends Trump {
-  def getMask():Long={
+  def getMask(): Long = {
+
+    // 0000FF00 = Hearts
+    // 10101010 = Jacks
+    // 1010FF10 = Hearts | Jacks
+
     // 0000 0000 0000 0000 1111 1111 0000 0000 = 65280L = Hearts
     // 0001 0000 0001 0000 0001 0000 0001 0000 = 269488144L = Jacks
     // 0001 0000 0001 0000 1111 1111 0001 0000 = 269549328L = Hearts | Jacks
-    return 269549328L
+    return 0x1010FF10L
   }
-  
-  def getSuit():Int= {
+
+  def getSuit(): Int = {
     return 1
   }
-  override def toString="Hearts"
+  override def toString = "Hearts"
 }
 /**
  * @author Oliver Friedrich
  *
  */
 case class Spades extends Trump {
-  def getMask():Long={
+  def getMask(): Long = {
     // 0000 0000 1111 1111 0000 0000 0000 0000 = 16711680L = Spades
     // 0001 0000 0001 0000 0001 0000 0001 0000 = 269488144L = Jacks
     // 0001 0000 1111 1111 0001 0000 0001 0000 = 285151248L = Spades + Jacks
     return 285151248L
   }
-  
-  def getSuit():Int= {
+
+  def getSuit(): Int = {
     2
   }
-  
-  override def toString="Spades"
+
+  override def toString = "Spades"
 }
 /**
  * @author Oliver Friedrich
  *
  */
 case class Clubs extends Trump {
-  def getMask():Long={
+  def getMask(): Long = {
+
+    // FF000000 = Clubs
+    // 10101010 = Jacks
+    // FF101010 = Clubs | Jacks
+
     // 1111 1111 0000 0000 0000 0000 0000 0000 = 4278190080L = Clubs
     // 0001 0000 0001 0000 0001 0000 0001 0000 = 269488144L = Jacks
-    // 1111 1111 0001 0000 0001 0000 0001 0000 = 4279242768L = Clubs + Jacks
-    return 4279242768L
+    // 1111 1111 0001 0000 0001 0000 0001 0000 = 4279242768L = Clubs | Jacks
+    return 0xFF101010L
   }
-  
-  def getSuit():Int= {
+
+  def getSuit(): Int = {
     return 3
   }
-  
-  override def toString="Clubs"
+
+  override def toString = "Clubs"
 }
 /**
  * @author Oliver Friedrich
  *
  */
 case class Null extends Trump {
-  def getMask():Long={
+  def getMask(): Long = {
     return 0L
   }
-  
-  def getSuit():Int= {
-   return 5 
+
+  def getSuit(): Int = {
+    return 5
   }
-  
-  
-  
-  override def toString="Null"
+
+  override def toString = "Null"
 }
 /**
  * @author Oliver Friedrich
  *
  */
 case class Grand extends Trump {
-  def getMask():Long={
+  def getMask(): Long = {
+    // 10101010 = Jacks
     // 00010000000100000001000000010000 = 269488144L = Jacks 
-    return 269488144L
+    return 0x10101010L
   }
-  
-  def getSuit():Int= {
-   return 4 
+
+  def getSuit(): Int = {
+    return 4
   }
-  
-  override def toString="Grand"
+
+  override def toString = "Grand"
 }
 /**
  * @author Oliver Friedrich
  *
  */
 case class Ramsch extends Trump {
-  def getMask():Long={
+  def getMask(): Long = {
+    // 10101010 = Jacks
     // 00010000000100000001000000010000 = 269488144L = Jacks
-    return 269488144L
+    return 0x10101010L
   }
-  
-  def getSuit():Int= {
+
+  def getSuit(): Int = {
     return 6
   }
-  
-  
-  override def toString="Ramsch"
+
+  override def toString = "Ramsch"
 }
